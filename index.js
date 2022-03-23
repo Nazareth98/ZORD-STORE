119d3f7a7ded5366f32b78722b65739409d39391
Nazareth98 <patrick98live@gmail.com>
2022-03-23 08:29:53 -0300

    Att

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const body = document.querySelector("#body");
 const posicao = body.getBoundingClientRect();
-const nav = document.querySelector("#nav");
+const nav = document.querySelector('#nav')
+const cardsBg = document.querySelector('#cards-bg')
 
-body.addEventListener("click", (event) => {
-  console.log(event.pageY);
-  posicaoNav(event.pageY);
-});
+const isOnScreen = () => {
+      let rect = cardsBg.getBoundingClientRect()
+      return rect.top > 0 && rect.bottom  * 0.7 < window.innerHeight;
+}
 
-function posicaoNav(posicaoY) {
-  if (posicaoY <= 800) {
-    nav.classList.add("nav-scroll");
+function animaNavBar() { 
+  if (isOnScreen()) {
+    nav.classList.add("nav-scroll")
   }
 }
-console.log(nav);
+//const animar = animaNavBar(cardsBg)
+
+window.addEventListener('scroll', animaNavBar)
